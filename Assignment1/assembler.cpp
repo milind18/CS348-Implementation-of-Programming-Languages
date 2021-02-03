07c0006e083cc45bca4e47870158f77f296258c9
milind18 <53634719+milind18@users.noreply.github.com>
2021-02-03 18:23:41 +0530

    Update assembler.cpp

--- Assignment1/assembler.cpp
+++ Assignment1/assembler.cpp
@@ -47,105 +47,100 @@ void init_opcode()
     return;
 }
 
-// function to parse each line of .asm file
-void parse_line(string line, vector<string> &parsed)
+//Function to strip spaces
+string remove_spaces(string a)
 {
-    stringstream checkcomment(line);
-    string first_word;
-    checkcomment >> first_word;
-    if (first_word[0] == '.')
+    string ans;
+    int start = 0;
+    for (int i = 0; i < a.size(); i++)
     {
-        parsed[0] = ".";
-        return;
-    }
-    stringstream ss(line);
-    string word;
-    int i = 1;
-
-    while (ss >> word)
-    {
-        parsed[i] = word;
-        i++;
-    }
-    if (i == 3)
-    {
-        parsed[3] = parsed[2];
-        parsed[2] = parsed[1];
-        parsed[1] = "-";
-    }
-    else if (i == 2)
-    {
-        parsed[2] = parsed[1];
-        parsed[3] = parsed[1] = "-";
-    }
-    return;
-}
-
-// function to parse each line of intermmediate file
-void parse_line2(string line, vector<string> &parsed)
-{
-    stringstream checkcomment(line);
-    string first_word;
-    checkcomment >> first_word;
-    if (first_word[0] == '.')
-    {
-        parsed[0] = ".";
-        return;
-    }
-    stringstream ss(line);
-    string word;
-    int i = 1;
-
-    while (ss >> word)
-    {
-        parsed[i] = word;
-        i++;
-    }
-    if (parsed[1][0] == '.')
-    {
-        parsed[0] = ".";
+        if (a[i] == ' ')
+        {
+            start++;
+        }
+        else
+        {
+            break;
+        }
     }
 
-    return;
+    int end = a.size() - 1;
+
+    for (int i = a.size() - 1; i >= 0; i--)
+    {
+        if (a[i] == ' ')
+        {
+            end--;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return a.substr(start, end - start + 1);
 }
 
 // function to read lines from the .asm file
-int read_line(ifstream &fin, vector<string> &parsed, string &code_line)
+int read_line(ifstream &fin, string &code_line)
 {
     string line;
     if (!getline(fin, line))
     {
-        return 0;
+        return -1;
     }
     code_line = line;
-    for (int i = 0; i < 4; i++)
+
+    label = "";
+    operand = "";
+    operation = "";
+    iscomment = false;
+
+    stringstream checkcomment(line);
+    string first_word;
+    checkcomment >> first_word;
+    if (first_word[0] == '.')
     {
-        parsed[i] = "";
+        iscomment = true;
+        return 1;
     }
-    parse_line(line, parsed);
-    return 1;
+    label = remove_spaces(line.substr(0, 10));
+    operation = remove_spaces(line.substr(10, 10));
+    operand = remove_spaces(line.substr(20));
+
+    return 0;
 }
 
-//function to read lines from the intermmediate file
-int read_line2(ifstream &fin, vector<string> &parsed, string &code_line)
+// function to read lines from intermediate file
+int read_line2(ifstream &fin, string &code_line)
 {
     string line;
     if (!getline(fin, line))
     {
-        return 0;
+        return -1;
     }
     code_line = line;
-    for (int i = 0; i < 5; i++)
+
+    addr = "";
+    label = "";
+    operand = "";
+    operation = "";
+    iscomment = false;
+
+    stringstream checkcomment(line);
+    string first_word;
+    checkcomment >> first_word;
+    if (first_word[0] == '.')
     {
-        parsed[i] = "";
+        iscomment = true;
+        return 1;
     }
-    parse_line2(line, parsed);
-    iscomment = (parsed[0] == ".");
-    addr = parsed[1];
-    label = parsed[2];
-    operation = parsed[3];
-    operand = parsed[4];
-    return 1;
+    addr = remove_spaces(line.substr(0, 10));
+    label = remove_spaces(line.substr(10, 10));
+    operation = remove_spaces(line.substr(20, 10));
+    operand = remove_spaces(line.substr(30));
+
+    return 0;
 }
 
 //converting  a hexadecimal string to a long long int
@@ -219,7 +214,7 @@ long long num(string a)
     return ans;
 }
 
-// function to add a prefix of zeroes to make the total length of string a 'length'
+// function to add a prefix of zeroes to make the total length of string 'a' =  'length'
 string pad_zero(string a, int length = 6)
 {
     int k = a.size();
@@ -244,10 +239,10 @@ string pad_zero(string a, int length = 6)
 // Function to process operand and check if it contains a symbol
 string isSymbol(string operand)
 {
-    if (operand == "-")
+    if (operand == "")
         return "";
     int n = operand.size();
-    if (n - 2 >= 0 && operand[n - 2] == ',')
+    if (n - 2 >= 0 && operand[n - 2] == ',' && operand[n - 1] == 'X')
     {
         return operand.substr(0, n - 2);
     }
@@ -301,6 +296,7 @@ int main(int argc, char *argv[])
         cout << "Provide assembly file name as command line argument\n";
         exit(0);
     }
+
     ifstream fin;
     ofstream fout;
     ofstream listing;
@@ -310,76 +306,75 @@ int main(int argc, char *argv[])
         cout << "File not found in directory\n";
         exit(0);
     }
-    fout.open("intermmediate.txt");
+    fout.open("intermediate.txt");
     listing.open("listing.txt");
     init_opcode();
 
     //Pass 1
-    vector<string> parsed(4);
 
     //read first line
     string code_line;
-    read_line(fin, parsed, code_line);
-    if (parsed[2] == "START")
+    read_line(fin, code_line);
+    if (operation == "START")
     {
-        prog_name = parsed[1];
-        start_address = parsed[3];
+        prog_name = label;
+        start_address = operand;
         loc_ctr = start_address;
         //Write line
-        fout << left << setw(8) << loc_ctr << setw(8) << parsed[1] << setw(8) << parsed[2] << setw(8) << parsed[3] << endl;
-        read_line(fin, parsed, code_line);
+        fout << left << setw(10) << loc_ctr << setw(10) << label << setw(10) << operation << setw(30) << operand << endl;
+        read_line(fin, code_line);
     }
     else
     {
         loc_ctr = "0000";
     }
     //While opcode ! = END do
-    while (parsed[2] != "END")
+    while (operation != "END")
     {
         //If this is not a comment line
-        if (parsed[0] != ".")
+        if (!iscomment)
         {
             //If there is a symbol in the label field
-            if (parsed[1] != "" && parsed[1] != "-")
+            if (label != "")
             {
                 bool found_sym;
                 //Search for symbol
-                if (symtab.find(parsed[1]) != symtab.end())
+                if (symtab.find(label) != symtab.end())
                 {
                     found_sym = true;
-                    //cout << "Symbol found: " << parsed[1] << endl;
+                    //cout << "Symbol found: " << label << endl;
                 }
                 else
                 {
                     found_sym = false;
-                    //cout << "Symbol not found: " << parsed[1] << endl;
+                    //cout << "Symbol not found: " << label << endl;
                 }
 
                 if (found_sym)
                 {
                     //!Set error flag
-                    cerr << "Error: Multiple definitions of symbol: " << parsed[1] << endl;
-                    cout << code_line;
+                    cerr << "Error: Multiple definitions of symbol: " << label << endl;
+                    //cout << code_line;
                 }
                 else
                 {
-                    symtab.insert({parsed[1], loc_ctr});
+                    symtab.insert({label, loc_ctr});
                 }
             }
             //Search optab for opcode
 
             bool found_opcode;
             string opcode;
-            if (optab.find(parsed[2]) != optab.end())
+            if (optab.find(operation) != optab.end())
             {
                 found_opcode = true;
-                opcode = optab[parsed[2]];
-                //cout << "Opcode found: " << parsed[2] << endl;
+                opcode = optab[operation];
+                //cout << "Opcode found: " << operation << endl;
             }
             else
             {
                 found_opcode = false;
-                //cout << "Opcode not found: " << parsed[2] << endl;
+                //cout << "Opcode not found: " << operation << endl;
             }
             curr_ctr = loc_ctr;
             if (found_opcode == true)
@@ -387,25 +382,25 @@ int main(int argc, char *argv[])
                 // Add 3* instruction length to opcode
                 loc_ctr = to_hex(from_hex(loc_ctr) + 3);
             }
-            else if (parsed[2] == "WORD")
+            else if (operation == "WORD")
             {
                 // Add 3 to loc_ctr
                 loc_ctr = to_hex(from_hex(loc_ctr) + 3);
             }
-            else if (parsed[2] == "RESW")
+            else if (operation == "RESW")
             {
                 //Add 3*#[Operand] to loc_ctr
-                loc_ctr = to_hex(from_hex(loc_ctr) + 3 * num(parsed[3]));
+                loc_ctr = to_hex(from_hex(loc_ctr) + 3 * num(operand));
             }
-            else if (parsed[2] == "RESB")
+            else if (operation == "RESB")
             {
-                loc_ctr = to_hex(from_hex(loc_ctr) + num(parsed[3]));
+                loc_ctr = to_hex(from_hex(loc_ctr) + num(operand));
             }
-            else if (parsed[2] == "BYTE")
+            else if (operation == "BYTE")
             {
                 //find length of constant in bytes
-                int constLength = parsed[3].size() - 3;
-                if (parsed[3][0] == 'X')
+                int constLength = operand.size() - 3;
+                if (operand[0] == 'X')
                 {
                     constLength = (constLength + 1) / 2;
                 }
@@ -415,53 +410,53 @@ int main(int argc, char *argv[])
             else
             {
                 //!set error flag (invalid opcode)
-                cerr << "Error: Operation not found";
+                cout << "Error: Operation not found" << code_line << operation;
                 exit(0);
             }
         }
         //write line to intermediate file
-        if (parsed[0] == ".")
+        if (iscomment)
         {
             fout << code_line << endl;
         }
         else
         {
-            fout << left << setw(8) << curr_ctr << setw(8) << parsed[1] << setw(8) << parsed[2] << setw(8) << parsed[3] << endl;
+            fout << left << setw(10) << curr_ctr << setw(10) << label << setw(10) << operation << setw(30) << operand << endl;
         }
 
-        read_line(fin, parsed, code_line);
+        read_line(fin, code_line);
     }
     //write last line
-    fout << left << setw(8) << loc_ctr << setw(8) << parsed[1] << setw(8) << parsed[2] << setw(8) << parsed[3] << endl;
+    fout << left << setw(10) << loc_ctr << setw(10) << label << setw(10) << operation << setw(30) << operand << endl;
 
     //update prog_length
     long long prog_length = from_hex(loc_ctr) - from_hex(start_address);
     //End of pass 1
     fout.close();
     fin.close();
-    cout << "Intermmediate file created...\n";
-    //Pass 2
-    fin.open("intermmediate.txt");
-    fout.open("objectcode.txt");
-    parsed.resize(5);
-    read_line2(fin, parsed, code_line);
+    cout << "Intermediate file created...\n";
 
-    //Need to fix this if START does not exist
+    //Pass 2
+    fin.open("intermediate.txt");
+    fout.open("objectcode.txt");
+    read_line2(fin, code_line);
+
+    //Printing header
     fout << left << "H" << setw(6) << prog_name << pad_zero(start_address) << pad_zero(to_hex(prog_length)) << endl;
-    if (parsed[3] == "START")
+    if (operand == "START")
     {
         //Write listing line
         listing << code_line << endl;
-        read_line2(fin, parsed, code_line);
+        read_line2(fin, code_line);
     }
 
     stringstream textrecord;
     textrecord.str("");
     stringstream objcode;
     string begin_addr;
+
     while (operation != "END")
     {
-        //cout << iscomment;
         if (iscomment == false)
         {
             bool found_opcode;
@@ -506,11 +501,6 @@ int main(int argc, char *argv[])
                 if (operation == "WORD")
                 {
                     string newoperand = pad_zero(to_hex(stoi(operand)));
-                    if (newoperand.size() > 6)
-                    {
-                        cerr << "Error: Size of integer constant too large to fit in a word \n";
-                        exit(0);
-                    }
                     objcode << newoperand;
                 }
                 else if (operation == "BYTE")
@@ -569,7 +559,7 @@ int main(int argc, char *argv[])
             listing << code_line << endl;
         }
 
-        read_line2(fin, parsed, code_line);
+        read_line2(fin, code_line);
     }
     if (textrecord.str() != "")
     {
